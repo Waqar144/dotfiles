cf974b099a0ce4ffae4492d5b076e04c44a00465
Waqar Ahmed <waqar.17a@gmail.com>
2020-09-15 19:52:37 +0500

    Initial

---
+++ config.fish
@@ -0,0 +1,10 @@
+
+alias vi='nvim'
+alias ls='lsd'
+alias l='lsd -l'
+alias cmaked='cmake -DCMAKE_BUILD_TYPE=Debug'
+
+set theme_display_date no
+set fish_prompt_pwd_dir_length 0
+set theme_display_git_master_branch yes
+
